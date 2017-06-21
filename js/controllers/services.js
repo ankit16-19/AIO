67f37ff1ee6f8fc2b29730ad3c87cc2c92c8ac97
Ankit <ankitsocial16@gmail.com>
2017-06-21 15:24:31 +0530

    test6

--- js/controllers/services.js
+++ js/controllers/services.js
@@ -87,7 +87,7 @@ angular
           return $http.post('http://localhost:3001/api/hibi' + path,data);
 
         }, function errorCallback(response){
-          // do something when errorCallback // test5
+          // do something when errorCallback // test6
           return "error in processing the request"
 
         })
