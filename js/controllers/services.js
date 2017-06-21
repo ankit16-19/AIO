740c13d1e4f0ac81f6fc6888bf575e13b77d7c1f
Ankit Choudhary <ankitsocial16@gmail.com>
2017-06-21 12:52:02 +0530

    sublime test

--- js/controllers/services.js
+++ js/controllers/services.js
@@ -87,7 +87,7 @@ angular
           return $http.post('http://localhost:3001/api/hibi' + path,data);
 
         }, function errorCallback(response){
-          // do something when error
+          // do something when error 
           return "error in processing the request"
 
         })
